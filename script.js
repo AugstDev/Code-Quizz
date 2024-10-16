4205058093a1d7561cd09d03174b40052510a728
AugstDev <igoraugsa@gmail.com>
2024-10-16 03:01:56 -0300

    sistema de resposta + css + js

--- script.js
+++ script.js
@@ -1,18 +1,101 @@
 const questions = [
     {
-        title: "Qual a cor da Maçã?",
-        time: 10,
+        title: "Qual destas é uma linguagem de programação?",
+        time: 60,
         answers: [
-            ["Azul", false],
-            ["Marrom", false],
-            ["Laranja", false],
-            ["Vermelha", true]]
-        },
-]
+            ["HTML", false],
+            ["CSS", false],
+            ["JavaScript", true],
+            ["SQL", false]]
+    },
+    {
+        title: "O que significa 'CSS'?",
+        time: 60,
+        answers: [
+            ["Cascading Style Sheets", true],
+            ["Computer Style System", false],
+            ["Creative Style Syntax", false],
+            ["Complex Script Styles", false]]
+    },
+    {
+        title: "Qual dessas estruturas de controle é um loop?",
+        time: 60,
+        answers: [
+            ["if", false],
+            ["for", true],
+            ["switch", false],
+            ["try", false]]
+    },
+    {
+        title: "Qual destes operadores é usado para atribuição em JavaScript?",
+        time: 60,
+        answers: [
+            ["==", false],
+            ["=", true],
+            ["===", false],
+            ["!=", false]]
+    },
+    {
+        title: "Qual desses frameworks é usado com JavaScript?",
+        time: 60,
+        answers: [
+            ["React", true],
+            ["Laravel", false],
+            ["Django", false],
+            ["Spring", false]]
+    },
+    {
+        title: "Em qual linguagem usamos 'print()' para exibir algo no console?",
+        time: 60,
+        answers: [
+            ["Java", false],
+            ["Python", true],
+            ["C++", false],
+            ["JavaScript", false]]
+    },
+    {
+        title: "O que 'NaN' representa em JavaScript?",
+        time: 60,
+        answers: [
+            ["Null", false],
+            ["Not a Number", true],
+            ["New Array Number", false],
+            ["Negative Answer Null", false]]
+    },
+    {
+        title: "O que 'DOM' significa em desenvolvimento web?",
+        time: 60,
+        answers: [
+            ["Document Object Model", true],
+            ["Data Output Method", false],
+            ["Direct Object Manipulation", false],
+            ["Dynamic Operation Model", false]]
+    },
+    {
+        title: "Qual comando é usado para criar uma função em JavaScript?",
+        time: 60,
+        answers: [
+            ["def", false],
+            ["function", true],
+            ["fun", false],
+            ["func", false]]
+    },
+    {
+        title: "O que 'Git' é utilizado para?",
+        time: 60,
+        answers: [
+            ["Controlar versões de código", true],
+            ["Executar testes automatizados", false],
+            ["Criar interfaces gráficas", false],
+            ["Gerenciar bancos de dados", false]]
+    }
+];
+
+
 
 let correctAnswer
-let timeLeft = 120;
-
+let timeLeft = 0;
+let countCorrect = 0;
 
 
 function formatTime(seconds){
@@ -23,31 +106,38 @@ function formatTime(seconds){
 }
 
 
-function isCorrect(id){
+function isCorrect(id,questionNumber){
 
-    alert(questions[0].answers[id][1])
-    if(questions[0].answers[id][1] === true){
+    if(questions[questionNumber].answers[id][1] === true){
         return true;
     }else{
         return false;
     }
 }
 
+function getNextQuestion(actualQuestion){
+    try {
+        setQuestion(actualQuestion + 1);
+    } catch (error) {
+        alert(`Você acertou ${countCorrect}/${actualQuestion + 1} questões!`);
+    }
+}
 
 
 
-function setQuestion(){
+
+function setQuestion(number){
+    let questionNumber = number;
+    const playButton = document.getElementById("playButton");
     const title = document.getElementById("title");
     const answers = document.getElementById("answers_tab");
     const time = document.getElementById("time");
-    const reply = document.getElementById("replyText");
-    reply.textContent = '';
-    title.textContent = questions[0].title;
-    timeLeft = questions[0].time
+    title.textContent = questions[questionNumber].title;
+    timeLeft = questions[questionNumber].time
     time.textContent = formatTime(timeLeft);
     answers.innerHTML = '';
-    
-    questions[0].answers.forEach(function(answer, index){
+    playButton.style.display = 'none';
+    questions[questionNumber].answers.forEach(function(answer, index){
         
         if(answer[1] === true){
             correctAnswer = index;
@@ -56,15 +146,21 @@ function setQuestion(){
         const buttonElement = document.createElement("button");
         
         buttonElement.addEventListener("click", function(e){
-            
-            if(isCorrect(parseInt(e.target.id))){
-                reply.textContent = "Você Acertou!";
+            if(isCorrect(parseInt(e.target.id),questionNumber)){
+                buttonElement.style.animation = "correct 1s 1 ease-in-out forwards";
+                countCorrect++;
             }else {
-                reply.textContent = "Você Errou!";
+                buttonElement.style.animation = "wrong 1s 1 ease-in-out forwards";
             }
-        }
-        )
+        
+            buttonElement.addEventListener("animationend", (event) => {
+                clearInterval(timer);
+                getNextQuestion(questionNumber);
+            });
+        })
+        
         buttonElement.textContent = answer[0];
+        buttonElement.className = "answer"
         buttonElement.id = index
         answers.append(buttonElement);
     })
@@ -75,8 +171,9 @@ function setQuestion(){
         time.textContent = formatTime(timeLeft);
     
         if(timeLeft <= 0){
-            time.textContent = "Time's Up!";
+            time.textContent = "Acabou o Tempo!";
             clearInterval(timer);
+            getNextQuestion(questionNumber);
         }}, 1000)
 
 }
